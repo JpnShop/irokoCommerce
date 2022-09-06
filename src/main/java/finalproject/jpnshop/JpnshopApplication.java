1a08c84e9928679515c75504c43d80ffece9044a
Ramda_lee <rkdgml357@naver.com>
2022-09-06 22:10:03 +0900

    환경 설정  완료

---
+++ src/main/java/finalproject/jpnshop/JpnshopApplication.java
@@ -0,0 +1,13 @@
+package finalproject.jpnshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpnshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpnshopApplication.class, args);
+	}
+
+}

935772647e335d755677eac168d1cd2f5095bdd2
wijoonwu <devjoon@nate.com>
2022-09-14 17:02:04 +0900

    리뷰 전체 조회 구현

--- src/main/java/finalproject/jpnshop/biz/service/ReviewService.java
+++ src/main/java/finalproject/jpnshop/biz/service/ReviewService.java
@@ -4,11 +4,15 @@ import finalproject.jpnshop.biz.domain.Product;
 import finalproject.jpnshop.biz.domain.Review;
 import finalproject.jpnshop.biz.repository.ProductRepository;
 import finalproject.jpnshop.biz.repository.ReviewRepository;
+import finalproject.jpnshop.web.dto.ResReview;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -18,8 +22,13 @@ public class ReviewService {
     private final ProductRepository productRepository;
 
     //리뷰 전체조회
-    public List<Review> getReviews() {
-        return reviewRepository.findAll();
+    public List<ResReview.Response> getReviews() {
+         List<Review> reviews = reviewRepository.findAll();
+         List<ResReview.Response> responseList = new ArrayList<>();
+         for(Review review : reviews){
+             responseList.add(ResReview.Response.of(review));
+         }
+         return responseList;
     }
 
     //상품별 리뷰 조회

c122e8d490b3642b5c5ac2832e32100159716121
wijoonwu <devjoon@nate.com>
2022-09-14 17:01:11 +0900

    URL 수정

--- src/main/java/finalproject/jpnshop/web/controller/ReviewController.java
+++ src/main/java/finalproject/jpnshop/web/controller/ReviewController.java
@@ -2,50 +2,51 @@ package finalproject.jpnshop.web.controller;
 
 import finalproject.jpnshop.biz.domain.Review;
 import finalproject.jpnshop.biz.service.ReviewService;
+import finalproject.jpnshop.web.dto.ResReview;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/customers/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
 
     private final ReviewService reviewService;
 
     //리뷰 전체 목록 조회
-    @GetMapping
-    public List<Review> getReviews(){
+    @GetMapping("/customers/reviews")
+    public List<ResReview.Response> getReviews(){
         return reviewService.getReviews();
     }
 
     //리뷰 단건 조회
-    @GetMapping("/{reviewId}")
+    @GetMapping("/customers/reviews/{reviewId}")
     public Review getReview(@PathVariable long reviewId){
         return reviewService.getReview(reviewId);
     }
 
+    //todo : ProductController로 기능 이동 필요
     //상품별 리뷰 조회
-    @GetMapping("/{productId}")
+    @GetMapping("/products/{productId}/reviews")
     public Review getReviewByProduct(@PathVariable long productId) {
         return reviewService.getReviewByProduct(productId);
     }
 
     //리뷰 등록
-    @PostMapping
+    @PostMapping("/customers/reviews")
     public String insertReview(){
         return "";
     }
 
     //리뷰 수정
-    @PutMapping
+    @PutMapping("/customers/reviews")
     public String updateReview(){
         return "";
     }
 
     //리뷰 삭제
-    @DeleteMapping
+    @DeleteMapping("/customers/reviews")
     public String deleteReview(){
         return "";
     }

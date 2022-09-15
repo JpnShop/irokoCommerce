fe4b760944c16777a909a63fc4ef790c8c0ea883
wijoonwu <devjoon@nate.com>
2022-09-15 20:25:55 +0900

    Create ResNotice.java

---
+++ src/main/java/finalproject/jpnshop/web/dto/ResNotice.java
@@ -0,0 +1,37 @@
+package finalproject.jpnshop.web.dto;
+
+import finalproject.jpnshop.biz.domain.Member;
+import finalproject.jpnshop.biz.domain.Notice;
+import finalproject.jpnshop.biz.domain.Product;
+import finalproject.jpnshop.biz.domain.Review;
+import finalproject.jpnshop.web.dto.ResReview.Response;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class ResNotice {
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Response {
+
+        private Member member;
+        private String title;
+        private String content;
+        private LocalDateTime createdDate;
+
+        public static Response of(Notice notice) {
+            return Response.builder()
+                .member(notice.getMember())
+                .title(notice.getTitle())
+                .content(notice.getContent())
+                .createdDate(notice.getCreatedDate())
+                .build();
+        }
+    }
+}
+

f8ec86cc8f50a338b59f5325730ddb631707c87f
wijoonwu <devjoon@nate.com>
2022-09-15 18:08:12 +0900

    에러 메시지 수정

--- src/main/java/finalproject/jpnshop/biz/domain/properties/ErrorCode.java
+++ src/main/java/finalproject/jpnshop/biz/domain/properties/ErrorCode.java
@@ -9,7 +9,7 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(404,"회원 정보를 찾을 수 없습니다."),
     PRODUCT_NOT_FOUND(404,"상품 정보를 찾을 수 없습니다."),
     REVIEW_NOT_FOUND(404, "리뷰를 찾을 수 없습니다." ),
-    INTERNAL_SERVER_ERROR(500, "서버 에러입니다. 서버 팀에 연락주세요!");
+    INTERNAL_SERVER_ERROR(500, "서버 에러가 발생했습니다. 고객센터로 문의 바랍니다.");
 
     private final int status;
     private final String msg;

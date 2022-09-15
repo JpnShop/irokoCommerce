838a57a25032a44bee73867d6da0f98f0f181a24
wijoonwu <devjoon@nate.com>
2022-09-15 17:49:01 +0900

    ExceptionHandler 적용

--- src/main/java/finalproject/jpnshop/biz/service/ReviewService.java
+++ src/main/java/finalproject/jpnshop/biz/service/ReviewService.java
@@ -1,8 +1,9 @@
 package finalproject.jpnshop.biz.service;
 
+import finalproject.jpnshop.biz.domain.properties.ErrorCode;
 import finalproject.jpnshop.biz.domain.Product;
 import finalproject.jpnshop.biz.domain.Review;
-import finalproject.jpnshop.biz.exception.NoMemberException;
+import finalproject.jpnshop.biz.exception.CustomException;
 import finalproject.jpnshop.biz.repository.MemberRepository;
 import finalproject.jpnshop.biz.repository.ProductRepository;
 import finalproject.jpnshop.biz.repository.ReviewRepository;
@@ -35,21 +36,23 @@ public class ReviewService {
     }
 
     public ResReview.Response getReviewByProduct(Long productId) {
-        Product product = productRepository.findById(productId).orElseThrow(RuntimeException::new);
+        Product product = productRepository.findById(productId).orElseThrow(
+            () -> new CustomException(ErrorCode.PRODUCT_NOT_FOUND));
         return Response.of(reviewRepository.findByProduct(product));
     }
 
 
     public ResReview.Response getReview(long reviewId) {
-        return Response.of(reviewRepository.findById(reviewId).orElseThrow(RuntimeException::new));
+        return Response.of(reviewRepository.findById(reviewId).orElseThrow(
+            () -> new CustomException(ErrorCode.REVIEW_NOT_FOUND)));
     }
 
     //todo : 상품 당 1건 리뷰 중복체크 및 사진 저장
     public void insertReview(ReqReview reviewForm, Long productId) {
         reviewForm.setMember(memberRepository.findById(1L).orElseThrow(
-            () -> new NoMemberException("해당하는 멤버가 없습니다.")
-        ));
-        reviewForm.setProduct(productRepository.findById(productId).orElseThrow(RuntimeException::new));
+        () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)));
+        reviewForm.setProduct(productRepository.findById(productId).orElseThrow(
+            () -> new CustomException(ErrorCode.PRODUCT_NOT_FOUND)));
         Review review = reviewForm.toEntity();
         reviewRepository.save(review);
     }

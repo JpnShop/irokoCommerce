d474b07127ac42475b64676959ec9886106de44f
wijoonwu <devjoon@nate.com>
2022-09-15 13:17:20 +0900

    Create MemberRepository.java

---
+++ src/main/java/finalproject/jpnshop/biz/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package finalproject.jpnshop.biz.repository;
+
+import finalproject.jpnshop.biz.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+}

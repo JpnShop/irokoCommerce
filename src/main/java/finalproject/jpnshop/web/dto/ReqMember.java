c6d7e47a4ea6b178b09f6b54242ca96645b44726
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-09-17 17:19:36 +0900

    Request Member Dto 생성

---
+++ src/main/java/finalproject/jpnshop/web/dto/ReqMember.java
@@ -0,0 +1,26 @@
+package finalproject.jpnshop.web.dto;
+
+import finalproject.jpnshop.biz.domain.Member;
+import finalproject.jpnshop.biz.domain.properties.Gender;
+import java.util.Date;
+import lombok.Getter;
+
+@Getter
+public class ReqMember {
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private Gender gender;
+    private Date birthInfo;
+    private String role;
+
+    public Member toEntity() {
+        return new Member(id, username, password, email, gender, birthInfo, role);
+    }
+
+    @Override
+    public String toString() {
+        return "ReqMember{" + "id=" + id +", username= " + username + ", password= " + password + ", email= " + email + ", gender= " + gender + ", birthInfo= " + birthInfo + ", role= " + role + '}';
+    }
+}

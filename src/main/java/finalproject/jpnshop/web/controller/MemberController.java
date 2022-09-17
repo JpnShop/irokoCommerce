6a299f03ffb22578893d323b7bb4401b850960fa
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-09-17 18:13:18 +0900

    Member 컨트롤러 회원가입

--- src/main/java/finalproject/jpnshop/web/controller/MemberController.java
+++ src/main/java/finalproject/jpnshop/web/controller/MemberController.java
@@ -1,8 +1,13 @@
 package finalproject.jpnshop.web.controller;
 
+import finalproject.jpnshop.biz.domain.Member;
 import finalproject.jpnshop.biz.repository.MemberRepository;
+import finalproject.jpnshop.biz.service.MemberService;
+import finalproject.jpnshop.web.dto.ReqMember;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberRepository memberRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final MemberService memberService;
 
+
+    @PostMapping("/users")
+    public String insertMember(@RequestBody ReqMember memberForm) {
+        memberService.insertMember(memberForm);
+        return "회원가입이 완료되었습니다.";
+    }
 
 }

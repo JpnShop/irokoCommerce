2a07697d42a44fe5277b85c1fb0ce25ac62c794d
wijoonwu <devjoon@nate.com>
2022-09-19 21:02:00 +0900

    답변 수정 기능 구현

--- src/main/java/finalproject/jpnshop/biz/repository/QuestionRepository.java
+++ src/main/java/finalproject/jpnshop/biz/repository/QuestionRepository.java
@@ -1,5 +1,6 @@
 package finalproject.jpnshop.biz.repository;
 
+import finalproject.jpnshop.biz.domain.Answer;
 import finalproject.jpnshop.biz.domain.Product;
 import finalproject.jpnshop.biz.domain.Question;
 import java.util.List;
@@ -8,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface QuestionRepository extends JpaRepository<Question,Long> {
 
     List<Question> findAllByProduct(Product product);
+
+    Question findByAnswer(Answer answer);
 }
--- src/main/java/finalproject/jpnshop/biz/service/AnswerService.java
+++ src/main/java/finalproject/jpnshop/biz/service/AnswerService.java
@@ -1,26 +1,23 @@
 package finalproject.jpnshop.biz.service;
 
 import finalproject.jpnshop.biz.domain.Answer;
-import finalproject.jpnshop.biz.domain.Product;
 import finalproject.jpnshop.biz.domain.Question;
 import finalproject.jpnshop.biz.domain.properties.ErrorCode;
 import finalproject.jpnshop.biz.exception.CustomException;
 import finalproject.jpnshop.biz.repository.AnswerRepository;
 import finalproject.jpnshop.biz.repository.MemberRepository;
-import finalproject.jpnshop.biz.repository.ProductRepository;
 import finalproject.jpnshop.biz.repository.QuestionRepository;
 import finalproject.jpnshop.web.dto.ReqAnswer;
-import finalproject.jpnshop.web.dto.ReqQuestion;
 import finalproject.jpnshop.web.dto.ResAnswer;
-import finalproject.jpnshop.web.dto.ResQuestion;
-import finalproject.jpnshop.web.dto.ResQuestion.Response;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
+@Slf4j
 @Service
 public class AnswerService {
 
@@ -48,10 +45,12 @@ public class AnswerService {
     public void insertAnswer(ReqAnswer answerForm, Long questionId){
         answerForm.setMember(memberRepository.findById(1L).orElseThrow(
             () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)));
-        answerForm.setQuestion(questionRepository.findById(questionId).orElseThrow(
-            () -> new CustomException(ErrorCode.QUESTION_NOT_FOUND)));
+        Question question = questionRepository.findById(questionId).orElseThrow(
+            () -> new CustomException(ErrorCode.QUESTION_NOT_FOUND));
         Answer answer = answerForm.toEntity();
         answerRepository.save(answer);
+        question.setAnswer(answer);
+        question.setAnswerStatus("답변 완료");
     }
 
     @Transactional
@@ -66,6 +65,9 @@ public class AnswerService {
     public void deleteAnswer(Long answerId){
         Answer answer = answerRepository.findById(answerId).orElseThrow(
             () -> new CustomException(ErrorCode.ANSWER_NOT_FOUND));
+        Question question = questionRepository.findByAnswer(answer);
+        question.setAnswer(null);
+        question.setAnswerStatus("미답변");
         answerRepository.delete(answer);
     }
 

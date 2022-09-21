28604510e99ed2414e45f42d023075e80b95949c
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-09-21 20:33:29 +0900

    Update MemberRepository.java

--- src/main/java/finalproject/jpnshop/biz/repository/MemberRepository.java
+++ src/main/java/finalproject/jpnshop/biz/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package finalproject.jpnshop.biz.repository;
 
 import finalproject.jpnshop.biz.domain.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {

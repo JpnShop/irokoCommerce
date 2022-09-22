cf85526605bb08de592e1b5e97ee0a0cd0e3c02c
wijoonwu <devjoon@nate.com>
2022-09-22 17:42:31 +0900

    Create MagazineRepository.java

---
+++ src/main/java/finalproject/jpnshop/biz/repository/MagazineRepository.java
@@ -0,0 +1,8 @@
+package finalproject.jpnshop.biz.repository;
+
+import finalproject.jpnshop.biz.domain.Magazine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MagazineRepository extends JpaRepository<Magazine, Long> {
+
+}

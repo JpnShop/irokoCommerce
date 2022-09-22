10fb6512d36fa186739b7f4baa72b76b6a90ecf1
Ramda_lee <rkdgml357@naver.com>
2022-09-23 03:41:55 +0900

    Authorization -> Access_Token 변경

--- src/main/java/finalproject/jpnshop/config/auth/jwt/JwtProperties.java
+++ src/main/java/finalproject/jpnshop/config/auth/jwt/JwtProperties.java
@@ -4,5 +4,5 @@ public interface JwtProperties {
     String SECRET = "jpnshop";
     int EXPIRATION_TIME = 60000*10;
     String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Authorization";
+    String HEADER_STRING = "Access Token";
 }

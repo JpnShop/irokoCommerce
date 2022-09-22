bb8503bcf5138b62c20532cbaf5d9dbe7503f3f6
Ramda_lee <rkdgml357@naver.com>
2022-09-23 02:59:44 +0900

    DeliveryInfoRepository 생성

---
+++ src/main/java/finalproject/jpnshop/biz/repository/DeliveryInfoRepository.java
@@ -0,0 +1,8 @@
+package finalproject.jpnshop.biz.repository;
+
+import finalproject.jpnshop.biz.domain.DeliveryInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeliveryInfoRepository extends JpaRepository<DeliveryInfo, Long> {
+
+}

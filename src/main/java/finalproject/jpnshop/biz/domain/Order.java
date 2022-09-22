8f7daa7f6623f67fa13c1bbbc5ad194ba6079bbd
Ramda_lee <rkdgml357@naver.com>
2022-09-23 03:01:36 +0900

    주문 메서드 추가 생성

--- src/main/java/finalproject/jpnshop/biz/domain/Order.java
+++ src/main/java/finalproject/jpnshop/biz/domain/Order.java
@@ -1,7 +1,10 @@
 package finalproject.jpnshop.biz.domain;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import finalproject.jpnshop.biz.domain.properties.DeliveryStatus;
+import finalproject.jpnshop.biz.domain.properties.ErrorCode;
 import finalproject.jpnshop.biz.domain.properties.Status;
+import finalproject.jpnshop.biz.exception.CustomException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
@@ -9,20 +12,28 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Getter
 @Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order extends BaseTime {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,25 +41,19 @@ public class Order extends BaseTime {
     private Long id;
     @Enumerated(EnumType.STRING)
     private Status status;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private DeliveryInfo deliveryInfo;
+
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private final List<OrderItem> orderItems = new ArrayList<>();
 
-    protected Order() {
+    public Order(Status status, Member member, DeliveryInfo deliveryInfo, List<OrderItem> orderItems) {
+        super();
     }
 
-    public Order(Long id, Status status, Member member) {
-        this.id = id;
-        this.status = status;
-        this.member = member;
-    }
-
-    @Builder
-    public Order(Member member) {
-        this.member = member;
-    }
 
     public void addOrderItem(OrderItem orderItem) {
         this.orderItems.add(orderItem);
@@ -56,4 +61,49 @@ public class Order extends BaseTime {
             orderItem.setOrder(this);
         }
     }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setCreatedAt(LocalDateTime now) {
+    }
+
+    private void setDeliveryInfo(DeliveryInfo deliveryInfo) {
+    }
+
+    public static Order createOrder(Member member, DeliveryInfo deliveryInfo, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDeliveryInfo(deliveryInfo);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(Status.ORDER);
+        order.setCreatedAt(LocalDateTime.now());
+        return order;
+    }
+
+    public void cancel(OrderItem... orderItems) {
+        if (deliveryInfo.getStatus() == DeliveryStatus.COMPLETE) {
+            throw new CustomException(ErrorCode.ORDER_COMP_ERROR);
+        }
+        this.setStatus(Status.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
 }

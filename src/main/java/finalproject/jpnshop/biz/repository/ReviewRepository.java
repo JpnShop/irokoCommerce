1d7fb6e4a2e5075307758edbff46556794ce7103
Ramda_lee <rkdgml357@naver.com>
2022-09-23 03:48:45 +0900

    method 추가

--- src/main/java/finalproject/jpnshop/biz/repository/ReviewRepository.java
+++ src/main/java/finalproject/jpnshop/biz/repository/ReviewRepository.java
@@ -1,6 +1,7 @@
 package finalproject.jpnshop.biz.repository;
 
 
+import finalproject.jpnshop.biz.domain.Member;
 import finalproject.jpnshop.biz.domain.Product;
 import finalproject.jpnshop.biz.domain.Review;
 import java.util.List;
@@ -11,4 +12,6 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     Review findByProduct(Product product);
 
     List<Review> findAllByProduct(Product product);
+
+    List<Review> findByMember(Member member);
 }

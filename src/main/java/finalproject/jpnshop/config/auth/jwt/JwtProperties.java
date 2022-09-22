9fc7face3b41a7019abf492e41b6e3dc87b72fbd
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-09-22 14:17:03 +0900

    설정 변경으로 인한 클래스 파일들 제거

--- src/main/java/finalproject/jpnshop/config/auth/jwt/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package finalproject.jpnshop.config.auth.jwt;
-
-public interface JwtProperties {
-    String SECRET = "jpnshop";
-    int EXPIRATION_TIME = 60000*10;
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Access Token";
-}

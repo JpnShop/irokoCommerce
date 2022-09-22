c665e5d3c6c3d7d511f0ae598a1bb652c50e5b4f
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-09-22 18:11:05 +0900

    base64url 에러 수정

--- src/main/java/finalproject/jpnshop/jwt/JwtFilter.java
+++ src/main/java/finalproject/jpnshop/jwt/JwtFilter.java
@@ -35,7 +35,10 @@ public class JwtFilter extends OncePerRequestFilter {
     private String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
         if(StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
-            return bearerToken.substring(7);
+
+            String[] str = bearerToken.split(" ");
+            return str[1];
+
         }
         return null;
     }

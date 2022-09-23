e6d62e942de1ca0771ee118b068acd5a51378db3
Ramda_lee <rkdgml357@naver.com>
2022-09-23 13:59:29 +0900

    import 변경

--- src/main/java/finalproject/jpnshop/biz/repository/RefreshTokenRepository.java
+++ src/main/java/finalproject/jpnshop/biz/repository/RefreshTokenRepository.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByKey(String key);
 }

9f13d47a65912fc514329de3c3e2fe19e8ffd970
wijoonwu <devjoon@nate.com>
2022-09-27 14:42:49 +0900

    매거진 조회 기능 구현

--- src/main/java/finalproject/jpnshop/biz/service/MagazineService.java
+++ src/main/java/finalproject/jpnshop/biz/service/MagazineService.java
@@ -1,9 +1,14 @@
 package finalproject.jpnshop.biz.service;
 
 import finalproject.jpnshop.biz.domain.Magazine;
+import finalproject.jpnshop.biz.domain.Member;
 import finalproject.jpnshop.biz.domain.properties.ErrorCode;
 import finalproject.jpnshop.biz.exception.CustomException;
 import finalproject.jpnshop.biz.repository.MagazineRepository;
+import finalproject.jpnshop.biz.repository.MemberRepository;
+import finalproject.jpnshop.web.dto.ReqMagazine;
+import finalproject.jpnshop.web.dto.ResMagazine;
+import finalproject.jpnshop.web.dto.ResMagazine.Response;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,15 +20,22 @@ import org.springframework.transaction.annotation.Transactional;
 public class MagazineService {
 
     private final MagazineRepository magazineRepository;
+    private final MemberRepository memberRepository;
 
-    @Transactional
     public List<Magazine> getMagazines(){
         return magazineRepository.findAll();
     }
 
-    public void getMagazine(long id) {
+    public ResMagazine.Response getMagazine(long id) {
         Magazine magazine = magazineRepository.findById(id).orElseThrow(
             () -> new CustomException(ErrorCode.MAGAZINE_NOT_FOUND));
-
+        return Response.of(magazine);
     }
+
+    public void insertMagazine(ReqMagazine magazineForm){
+        Magazine magazine = magazineForm.toEntity();
+        magazineRepository.save(magazine);
+    }
+
+
 }

bfd59c859bcde1d31e4dfe3a08d1bc2ffcb01db7
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-10-04 17:01:45 +0900

    이용약관 Repository 생성

---
+++ src/main/java/finalproject/jpnshop/biz/repository/TermsRepository.java
@@ -0,0 +1,8 @@
+package finalproject.jpnshop.biz.repository;
+
+import finalproject.jpnshop.biz.domain.Terms;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TermsRepository extends JpaRepository<Terms, Long> {
+
+}

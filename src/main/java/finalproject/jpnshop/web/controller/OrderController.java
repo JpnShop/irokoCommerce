574f358858a2cddba479a0b0c31073a2d131a626
Ramda_lee <rkdgml357@naver.com>
2022-10-06 05:28:57 +0900

    Update OrderController.java
    OrderController 기능 수정

--- src/main/java/finalproject/jpnshop/web/controller/OrderController.java
+++ src/main/java/finalproject/jpnshop/web/controller/OrderController.java
@@ -1,52 +1,61 @@
 package finalproject.jpnshop.web.controller;
 
-import finalproject.jpnshop.biz.domain.Member;
+import finalproject.jpnshop.biz.service.DeliveryInfoService;
 import finalproject.jpnshop.biz.service.MemberService;
 import finalproject.jpnshop.biz.service.OrderService;
 import finalproject.jpnshop.biz.service.ProductService;
-import finalproject.jpnshop.web.dto.ResProduct.Response;
+import finalproject.jpnshop.web.dto.ResDeliveryInfo;
+import finalproject.jpnshop.web.dto.ResMember;
+import finalproject.jpnshop.web.dto.ResOrder;
+import finalproject.jpnshop.web.dto.ResOrder.Response;
+import finalproject.jpnshop.web.dto.ResProduct;
+import finalproject.jpnshop.web.dto.ResponseWrapper;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/order")
+@RequestMapping("/orders")
 public class OrderController {
     private final OrderService orderService;
     private final MemberService memberService;
     private final ProductService productService;
+    private final DeliveryInfoService deliveryInfoService;
 
-    @GetMapping
-    public void order(Model model, String username) {
-        List<Member> members = (List<Member>) memberService.getMember(username);
-        List<Response> products = productService.findProducts();
 
-        model.addAttribute("members", members);
-        model.addAttribute("products", products);
+    @GetMapping("/{deliveryId}")
+    public ResponseEntity<ResponseWrapper> getOrder(@PathVariable Long deliveryId, @RequestParam String username) {
 
+        ResMember.Response members = memberService.getMember(username);
+        List<ResProduct.Response> products = productService.findProducts();
+        ResDeliveryInfo.Response deliverys = deliveryInfoService.getDelivery(deliveryId);
+
+        return ResponseEntity.ok(new ResponseWrapper(members, products, deliverys));
     }
 
-    @PostMapping
-    public String order(Long memberId, Long productId, Long deliveryId, int count) {
-        orderService.createOrder(memberId, productId, deliveryId, count);
-        return "주문 신청이 완료 되었습니다.";
+    @PostMapping("/{memberId}")
+    public ResponseEntity<ResOrder.Response> createOrder(@PathVariable Long memberId, @RequestParam Long productId, @RequestParam Long deliveryId, @RequestParam Integer count) {
+        Long order = orderService.createOrder(memberId, productId, deliveryId, count);
+        Response orders = orderService.findOrder(order);
+        return ResponseEntity.ok(orders);
     }
 
     @GetMapping("/orderList")
-    public String orderList() {
-        orderService.findOrders();
-        return "주문 목록 입니다.";
+    public ResponseEntity<List<ResOrder.Response>> orderList() {
+        List<ResOrder.Response> orders = orderService.findOrders();
+        return ResponseEntity.ok(orders);
     }
 
     @PostMapping("/{orderId}/cancel")
-    public String cancelOrder(@PathVariable Long orderId) {
+    public ResponseEntity<ResOrder.Response> cancelOrder(@PathVariable Long orderId) {
         orderService.cancelOrder(orderId);
-        return "주문이 취소되었습니다.";
+        return ResponseEntity.ok().build();
     }
 }

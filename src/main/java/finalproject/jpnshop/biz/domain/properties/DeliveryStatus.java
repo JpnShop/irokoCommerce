b2c17f36b9c857490495c0c3fc4556e69c0313bd
Ramda_lee <rkdgml357@naver.com>
2022-10-06 05:27:31 +0900

    DeliveryStatus 수정
    Enum type 수정

--- src/main/java/finalproject/jpnshop/biz/domain/properties/DeliveryStatus.java
+++ src/main/java/finalproject/jpnshop/biz/domain/properties/DeliveryStatus.java
@@ -1,5 +1,15 @@
 package finalproject.jpnshop.biz.domain.properties;
 
+import lombok.Getter;
+
 public enum DeliveryStatus {
-    READY, COMPLETE
+    PAY("결제 완료"), SHIPPING("배송 중"), CANCEL("취소"), COMPLETE("배송 완료");
+
+    @Getter
+    public String korean;
+
+    DeliveryStatus(String kor) {
+        this.korean = kor;
+    }
+
 }

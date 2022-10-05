9cebd24ea5dc740e0b9d46c7f1c883733a80d62f
Ramda_lee <rkdgml357@naver.com>
2022-10-06 05:27:51 +0900

    Update DeliveryInfoRepository.java
    findOrder 생성

--- src/main/java/finalproject/jpnshop/biz/repository/DeliveryInfoRepository.java
+++ src/main/java/finalproject/jpnshop/biz/repository/DeliveryInfoRepository.java
@@ -1,8 +1,10 @@
 package finalproject.jpnshop.biz.repository;
 
 import finalproject.jpnshop.biz.domain.DeliveryInfo;
+import finalproject.jpnshop.biz.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DeliveryInfoRepository extends JpaRepository<DeliveryInfo, Long> {
 
+    DeliveryInfo findByOrder(Order order);
 }

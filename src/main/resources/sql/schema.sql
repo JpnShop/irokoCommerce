069ac543248e319ea6779ae7081109a69a86efe5
Ramda_lee <rkdgml357@naver.com>
2022-10-05 17:37:37 +0900

    .yml 추가
    application-auth.yml 추가

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,97 @@
+drop table if exists CartItem cascade;
+drop table if exists FavoriteItem cascade;
+drop table if exists MagazineItem cascade;
+drop table if exists OrderItem cascade;
+drop table if exists Question cascade;
+drop table if exists Review cascade;
+drop table if exists Product cascade;
+
+create table Product
+(
+    product_id      bigint  not null auto_increment,
+    createdAt       date,
+    modifiedAt      date,
+    brand           varchar(255),
+    brandImg        varchar(2000),
+    brandKo         varchar(255),
+    category        varchar(255),
+    detailList      varchar(2000),
+    detailThumbList varchar(2000),
+    price           integer not null,
+    productName     varchar(255),
+    productOption   varchar(255),
+    sale            integer,
+    stock           integer,
+    summary         varchar(255),
+    thumbnail       varchar(2000),
+    primary key (product_id)
+) engine = InnoDB;
+
+create table CartItem
+(
+    cart_item_id bigint  not null auto_increment,
+    createdAt    date,
+    modifiedAt   date,
+    count        integer not null,
+    cart_cart_id bigint,
+    product_id   bigint,
+    primary key (cart_item_id)
+) engine = InnoDB;
+
+create table FavoriteItem
+(
+    favorite_item_id     bigint not null auto_increment,
+    createdAt            date,
+    modifiedAt           date,
+    favorite_favorite_id bigint,
+    product_id           bigint,
+    primary key (favorite_item_id)
+) engine = InnoDB;
+
+create table MagazineItem
+(
+    magazine_item_id bigint not null auto_increment,
+    createdAt        date,
+    modifiedAt       date,
+    magazine_id      bigint,
+    product_id       bigint,
+    primary key (magazine_item_id)
+) engine = InnoDB;
+
+create table OrderItem
+(
+    order_item_id  bigint  not null auto_increment,
+    createdAt      date,
+    modifiedAt     date,
+    orderCount     integer not null,
+    orderPrice     integer not null,
+    order_order_id bigint,
+    product_id     bigint,
+    primary key (order_item_id)
+) engine = InnoDB;
+
+create table Question
+(
+    question_id  bigint  not null auto_increment,
+    answerStatus varchar(255),
+    content      longtext,
+    createdAt    datetime,
+    password     integer not null,
+    privateYn    varchar(255),
+    itle         varchar(255),
+    answer_id    bigint,
+    member_id    bigint,
+    product_id   bigint,
+    primary key (question_id)
+) engine = InnoDB;
+
+create table Review
+(
+    review_id   bigint not null auto_increment,
+    content     longtext,
+    createdDate datetime,
+    title       varchar(255),
+    member_id   bigint,
+    product_id  bigint,
+    primary key (review_id)
+) engine = InnoDB;

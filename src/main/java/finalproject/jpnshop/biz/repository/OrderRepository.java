32cfab2df96da84bdd7c58b524586bf72197e31d
wijoonwu <devjoon@nate.com>
2022-10-06 18:25:43 +0900

    주문한 사람만 리뷰 작성 기능 구현

--- src/main/java/finalproject/jpnshop/biz/repository/OrderRepository.java
+++ src/main/java/finalproject/jpnshop/biz/repository/OrderRepository.java
@@ -1,8 +1,12 @@
 package finalproject.jpnshop.biz.repository;
 
+import finalproject.jpnshop.biz.domain.Member;
 import finalproject.jpnshop.biz.domain.Order;
+import finalproject.jpnshop.biz.domain.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
 
+
+    Order findByMemberAndOrderItems(Member member, OrderItem orderItem);
 }

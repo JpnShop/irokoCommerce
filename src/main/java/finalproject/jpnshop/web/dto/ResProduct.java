0e5a6205e749440cde26cb8f6ae47aebf8b5d8fd
Ramda_lee <rkdgml357@naver.com>
2022-10-07 04:55:13 +0900

    Update ResProduct.java
    Dto 수정

--- src/main/java/finalproject/jpnshop/web/dto/ResProduct.java
+++ src/main/java/finalproject/jpnshop/web/dto/ResProduct.java
@@ -1,6 +1,8 @@
 package finalproject.jpnshop.web.dto;
 
 import finalproject.jpnshop.biz.domain.Product;
+import java.util.Arrays;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,19 +16,30 @@ public class ResProduct {
     @Builder
     public static class Response {
 
+        private Long productId;
+        private String brandImg;
+        private String tags;
+        private List<String> detailList;
+        private List<String> detailThumbList;
         private String productName;
+        private String productOption;
+        private int sale;
         private int price;
         private String brand;
         private int stock;
-        private String category;
+        private String thumbnail;
 
         public static Response of(Product product) {
             return Response.builder()
+                .productId(product.getId())
+                .brandImg(product.getBrandImg())
+                .tags(product.getTags())
+                .detailList(Arrays.asList(product.getDetailList().split(",")))
+                .detailThumbList(Arrays.asList(product.getDetailThumbList().split(",")))
                 .productName(product.getProductName())
                 .price(product.getPrice())
                 .brand(product.getBrand())
                 .stock(product.getStock())
-                .category(product.getTags())
                 .build();
         }
     }

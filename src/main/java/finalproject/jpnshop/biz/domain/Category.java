8413777c2bf3a54df40079e605c9a8b172be53a9
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-10-06 14:23:39 +0900

    Category 엔티티 생성

---
+++ src/main/java/finalproject/jpnshop/biz/domain/Category.java
@@ -0,0 +1,30 @@
+package finalproject.jpnshop.biz.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private final List<TopCategory> topCategories = new ArrayList<>();
+
+}

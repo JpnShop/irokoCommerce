6bfcaa7ca0a3cf87411f648d3ec16811de85e0ad
Ramda_lee <rkdgml357@naver.com>
2022-10-12 03:15:23 +0900

    Update ProductRepository.java

--- src/main/java/finalproject/jpnshop/biz/repository/ProductRepository.java
+++ src/main/java/finalproject/jpnshop/biz/repository/ProductRepository.java
@@ -4,5 +4,4 @@ import finalproject.jpnshop.biz.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
 }

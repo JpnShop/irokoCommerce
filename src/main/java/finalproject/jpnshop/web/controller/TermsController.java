cb1421fb483c2fbcb61e8107501a4299566425e8
Ramda_lee <rkdgml357@naver.com>
2022-10-12 04:02:01 +0900

    Update Code
    주석 제거

--- src/main/java/finalproject/jpnshop/web/controller/TermsController.java
+++ src/main/java/finalproject/jpnshop/web/controller/TermsController.java
@@ -1,25 +1,25 @@
-//package finalproject.jpnshop.web.controller;
-//
-//import finalproject.jpnshop.biz.service.TermsService;
-//import finalproject.jpnshop.web.dto.ResTerms.Response;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequiredArgsConstructor
-//public class TermsController {
-//
-//    private final TermsService termsService;
-//
-//    @GetMapping("/serviceTerms")
-//    public ResponseEntity<String> getServiceContent() {
-//        return ResponseEntity.ok(termsService.getServiceContent());
-//    }
-//
-//    @GetMapping("/personalTerms")
-//    public ResponseEntity<String> getPersonalContent() {
-//        return ResponseEntity.ok(termsService.getPersonalContent());
-//    }
-//}
+package finalproject.jpnshop.web.controller;
+
+import finalproject.jpnshop.biz.service.TermsService;
+import finalproject.jpnshop.web.dto.ResTerms.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TermsController {
+
+    private final TermsService termsService;
+
+    @GetMapping("/serviceTerms")
+    public ResponseEntity<String> getServiceContent() {
+        return ResponseEntity.ok(termsService.getServiceContent());
+    }
+
+    @GetMapping("/personalTerms")
+    public ResponseEntity<String> getPersonalContent() {
+        return ResponseEntity.ok(termsService.getPersonalContent());
+    }
+}

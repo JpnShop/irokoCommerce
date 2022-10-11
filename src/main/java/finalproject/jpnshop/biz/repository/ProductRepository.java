082cab11ffd42926f8b11c44aa422bb371b2e89c
Ramda_lee <rkdgml357@naver.com>
2022-10-12 04:37:27 +0900

    Update ProductRepository.java
    Query문 추가

--- src/main/java/finalproject/jpnshop/biz/repository/ProductRepository.java
+++ src/main/java/finalproject/jpnshop/biz/repository/ProductRepository.java
@@ -1,7 +1,13 @@
 package finalproject.jpnshop.biz.repository;
 
 import finalproject.jpnshop.biz.domain.Product;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query(" select p from Product p where p.id in :ids")
+    List<Product> findByIds(@Param("ids") List<Long> ids);
 }

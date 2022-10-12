b0e08765dddaf2387b60979fa3d0ed9eaae5fcc6
wndgndi <102509248+wndgndi@users.noreply.github.com>
2022-10-12 20:45:49 +0900

    SubCategory Repository 생성

---
+++ src/main/java/finalproject/jpnshop/biz/repository/SubCategoryRepository.java
@@ -0,0 +1,8 @@
+package finalproject.jpnshop.biz.repository;
+
+import finalproject.jpnshop.biz.domain.SubCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
+
+}

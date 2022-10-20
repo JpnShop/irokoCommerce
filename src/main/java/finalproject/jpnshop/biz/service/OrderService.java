1eb77b329d785a8b3b6de8f8cf197699a7868ab4
Ramda_lee <rkdgml357@naver.com>
2022-10-20 13:29:54 +0900

    Update OrderService.java
    비회원 기능 로직 추가

--- src/main/java/finalproject/jpnshop/biz/service/OrderService.java
+++ src/main/java/finalproject/jpnshop/biz/service/OrderService.java
@@ -1,18 +1,17 @@
 package finalproject.jpnshop.biz.service;
 
 import finalproject.jpnshop.biz.domain.DeliveryInfo;
-import finalproject.jpnshop.biz.domain.Member;
 import finalproject.jpnshop.biz.domain.Order;
-import finalproject.jpnshop.biz.domain.OrderItem;
-import finalproject.jpnshop.biz.domain.Product;
 import finalproject.jpnshop.biz.domain.properties.ErrorCode;
 import finalproject.jpnshop.biz.exception.CustomException;
 import finalproject.jpnshop.biz.repository.DeliveryInfoRepository;
 import finalproject.jpnshop.biz.repository.MemberRepository;
 import finalproject.jpnshop.biz.repository.OrderRepository;
 import finalproject.jpnshop.biz.repository.ProductRepository;
+import finalproject.jpnshop.util.SecurityUtil;
+import finalproject.jpnshop.web.dto.ReqDeliveryInfo;
+import finalproject.jpnshop.web.dto.ResDeliveryInfo;
 import finalproject.jpnshop.web.dto.ResOrder;
-import finalproject.jpnshop.web.dto.ResOrder.Response;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
@@ -32,41 +31,20 @@ public class OrderService {
 
     private final DeliveryInfoRepository deliveryInfoRepository;
 
-    @Transactional
-    public Long createOrder(Long memberId, Long productId, Integer count) {
 
-        Member member = memberRepository.findById(memberId).orElseThrow(
-            () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
-        Product product = productRepository.findById(productId).orElseThrow(
-            () -> new CustomException(ErrorCode.PRODUCT_NOT_FOUND));
-        DeliveryInfo deliveryInfo = new DeliveryInfo();
-        deliveryInfo.setAddress(deliveryInfo.getAddress());
-
-        OrderItem orderItem = OrderItem.createOrderItem(product, product.getPrice(), count);
-
-        Order order = Order.createOrder(member, deliveryInfo, orderItem);
-
-        orderRepository.save(order);
-
-        return order.getId();
-
+    public ResDeliveryInfo.Response createOrderInfo(ReqDeliveryInfo reqDeliveryInfo) {
+        DeliveryInfo deliveryInfo = reqDeliveryInfo.toEntity();
+        return ResDeliveryInfo.Response.of(deliveryInfo);
     }
 
-    public ResOrder.Response findOrder(Long orderId) {
+    @Transactional
+    public ResOrder.Response cancelOrder(Long orderId) {
+
         Order order = orderRepository.findById(orderId).orElseThrow(
             () -> new CustomException(ErrorCode.ORDER_NOT_FOUND)
         );
+
         return ResOrder.Response.of(order);
-    }
-
-    @Transactional
-    public void cancelOrder(Long orderId) {
-
-        Order order = orderRepository.findById(orderId).orElseThrow(
-            () -> new CustomException(ErrorCode.ORDER_NOT_FOUND)
-        );
-
-        order.cancel();
     }
 
     public List<ResOrder.Response> findOrders() {
@@ -76,6 +54,13 @@ public class OrderService {
             orderList.add(ResOrder.Response.of(order));
         }
         return orderList;
+    }
+
+    public boolean nonmember() {
+        if (memberRepository.existsById(SecurityUtil.getCurrentMemberId())) {
+            return true;
+        }
+        return false;
     }
 
 }

0dc2890f74e6bd7b979b2ccd6d89cf680bd4cb87
Ramda_lee <rkdgml357@naver.com>
2022-10-28 13:19:15 +0900

    Update Payment.java
    결제 엔티티 생성

--- src/main/java/finalproject/jpnshop/biz/domain/Payment.java
+++ src/main/java/finalproject/jpnshop/biz/domain/Payment.java
@@ -1,22 +1,22 @@
 package finalproject.jpnshop.biz.domain;
 
-import finalproject.jpnshop.biz.domain.properties.Coupon;
 import finalproject.jpnshop.biz.domain.properties.PayMethod;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class Payment extends BaseTime {
 
     @Id
@@ -24,11 +24,27 @@ public class Payment extends BaseTime {
     @Column(name = "payment_id")
     private Long id;
 
-    private Coupon coupon;
-
     private PayMethod payMethod;
-    //TODO
-    @OneToOne
+
+    private Integer count;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private Order order;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
+
+    @Builder
+    public Payment(PayMethod payMethod, Integer count, Product product) {
+        this.payMethod = payMethod;
+        this.count = count;
+        this.product = product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    public void setPayMethod(PayMethod payMethod) {
+        this.payMethod = payMethod;
+    }
 }

2982abbeb58f4258d4a32eb974549acd8d15544c
wijoonwu <devjoon@nate.com>
2022-10-28 15:29:26 +0900

    Update CartController.java

--- src/main/java/finalproject/jpnshop/web/controller/CartController.java
+++ src/main/java/finalproject/jpnshop/web/controller/CartController.java
@@ -1,5 +1,7 @@
 package finalproject.jpnshop.web.controller;
 
+import finalproject.jpnshop.biz.domain.properties.ErrorCode;
+import finalproject.jpnshop.biz.exception.CustomException;
 import finalproject.jpnshop.biz.service.CartService;
 import finalproject.jpnshop.util.SecurityUtil;
 import finalproject.jpnshop.web.dto.ResCartItem.Response;
@@ -23,8 +25,12 @@ public class CartController {
 
     @GetMapping
     public List<Response> getCarts(){
-        long memberId = SecurityUtil.getCurrentMemberId();
-        return cartService.getCarts(memberId);
+        try{
+            long memberId = SecurityUtil.getCurrentMemberId();
+            return cartService.getCarts(memberId);
+        } catch (Exception e){
+            throw new CustomException(ErrorCode.JWT_ERROR);
+        }
     }
 
     @PostMapping

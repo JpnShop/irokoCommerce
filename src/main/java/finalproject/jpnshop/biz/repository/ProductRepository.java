4266841ed5b2d1fdb27bc44734ece3427ffe5406
Ramda_lee <rkdgml357@naver.com>
2022-10-28 13:20:28 +0900

    Update ProductRepository.java
    결제 서비스 관련 쿼리 추가

--- src/main/java/finalproject/jpnshop/biz/repository/ProductRepository.java
+++ src/main/java/finalproject/jpnshop/biz/repository/ProductRepository.java
@@ -1,7 +1,9 @@
 package finalproject.jpnshop.biz.repository;
 
+import finalproject.jpnshop.biz.domain.Payment;
 import finalproject.jpnshop.biz.domain.Product;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +14,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByIds(@Param("ids") List<Long> ids);
     List<Product> findByTagsContaining(String tag);
 
+    @Query(" select p from Product p where p.payments in :pays")
+    List<Payment> findBy(@Param("pays") List<Long> pays);
+
+    Optional<Product> findById(Long productId);
 }
